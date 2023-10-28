e91006605dfefb60d4052820c6d9479828b3a04f
Nived Patel <nivedpatel08@gmail.com>
2023-10-28 21:06:39 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(ReminderApp());
+
+class ReminderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reminder App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: ReminderScreen(),
+    );
+  }
+}
+
+class ReminderScreen extends StatefulWidget {
+  @override
+  _ReminderScreenState createState() => _ReminderScreenState();
+}
+
+class audioplayer {
+  static play(String src) async {
+    final audioplayer = AudioPlayer();
+    await audioplayer.play(AssetSource(src));
+  }
+}
+
+class _ReminderScreenState extends State<ReminderScreen> {
+  List<String> daysOfWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  String selectedDay = 'Monday';
+  TimeOfDay selectedTime = TimeOfDay.now();
+  String selectedActivity = 'Wake up';
+
+  void _scheduleReminder() {
+    DateTime now = DateTime.now();
+    DateTime scheduledTime = DateTime(
+      now.year,
+      now.month,
+      now.day,
+      selectedTime.hour,
+      selectedTime.minute,
+    );
+
+    Duration duration = scheduledTime.isBefore(now)
+        ? scheduledTime.add(Duration(days: 1)).difference(now)
+        : scheduledTime.difference(now);
+
+    Future.delayed(duration, () {
+      audioplayer.play('notification.mp3');
+      // You can add additional actions here, such as displaying notifications.
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reminder App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DropdownButton<String>(
+              value: selectedDay,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedDay = newValue!;
+                });
+              },
+              items: daysOfWeek.map((String day) {
+                return DropdownMenuItem<String>(
+                  value: day,
+                  child: Text(day),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                TimeOfDay? picked = await showTimePicker(
+                  context: context,
+                  initialTime: selectedTime,
+                );
+                if (picked != null) {
+                  setState(() {
+                    selectedTime = picked;
+                  });
+                }
+              },
+              child: Text('Select Time: ${selectedTime.format(context)}'),
+            ),
+            SizedBox(height: 20),
+            DropdownButton<String>(
+              value: selectedActivity,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedActivity = newValue!;
+                });
+              },
+              items: <String>[
+                'Wake up',
+                'Go to gym',
+                'Breakfast',
+                'Meetings',
+                'Lunch',
+                'Quick nap',
+                'Go to library',
+                'Dinner',
+                'Go to sleep',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _scheduleReminder();
+              },
+              child: Text('Set Reminder'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
